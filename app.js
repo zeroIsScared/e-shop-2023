ae6bac2313e246c19b213c03dbe80cefdb50329a
zeroIsScared <69678222+zeroIsScared@users.noreply.github.com>
2023-09-08 10:57:35 +0300

    Update app.js
    removed some white spaces

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 import Fastify from 'fastify';
 import {productRoutes} from './routes/products.js';
-
 import {homePageRoute} from './routes/homePage.js';
 
- const fastify= Fastify({logger:true});
-
+const fastify= Fastify({logger:true});
 const PORT = {port: 3000};
 
  fastify.register(productRoutes, {prefix:'/products'});
@@ -13,12 +11,11 @@ const PORT = {port: 3000};
 const start =  async () =>{
     try{
         await fastify.listen(PORT);
-        fastify.log.info(`server listen on ${PORT.port}`)
+        fastify.log.info(`server listen on ${PORT.port}`);
     }catch (error) {
         fastify.log.error(error);
         process.exit(1);
-    }
-}
-
+    };
+};
 
 start();
