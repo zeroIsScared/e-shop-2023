00a2e70922eb920a197e103ae1031d720b080b2d
zeroIsScared <69678222+zeroIsScared@users.noreply.github.com>
2023-09-14 18:47:14 +0300

    Add files via upload
    Commit task 3

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ import Fastify from 'fastify';
 import {productRoutes} from './routes/products.js';
 import {homePageRoute} from './routes/homePage.js';
 import {clientsRoutes} from './routes/clients.js';
+import {authRoutes} from './auth/sing_in_routes.js';
 
  const fastify= Fastify({logger:true});
 const PORT = {port: 3000};
 
  fastify.register(productRoutes, {prefix:'/products'});
+ fastify.register(authRoutes, {prefix:'/auth'});
  fastify.register(homePageRoute);
  fastify.register(clientsRoutes, {prefix:'/clients'});
 
---
+++ auth/sing_in_routes.js
@@ -0,0 +1,54 @@
+import {pool} from '../databaseConnection.js';
+import {v4 as uuidv4} from 'uuid';
+
+export const authRoutes =async(fastify, options)=>{
+
+   
+    fastify.post('/signin', (req, rep) => {
+        console.log(req.body.email);
+        pool.query(`SELECT * FROM clients WHERE email= '${req.body.email}' AND password= '${req.body.password}';`, (err, res) => {
+console.log(res)
+         if (res.rows[0] === undefined) 
+         {
+            rep.code(401).send('Authorization failed!');
+
+         }
+         else if(!err )
+         {
+            const sessionId = uuidv4();
+            console.log(sessionId);
+            console.log(res.rows[0].id);
+            pool.query(`INSERT INTO client_sessions (session_id, client_id) VALUES('${sessionId}', ${res.rows[0].id});`,(error, response) => {
+                if (!error){
+
+                    rep.code(200).send({satus: 'ok', session_id: `${sessionId}`}); 
+
+                }else {
+                    console.log(error.message);
+
+                }                
+            })            
+        } else {            
+            console.log(err.message);
+        }      
+        });
+    });
+
+    fastify.delete('/signout', (req,rep) => {
+        console.log(req.body);
+        pool.query(`DELETE FROM client_sessions WHERE session_id = '${req.body.session_id}'`, (err, res) => {
+            if (res.rowCount === 0)
+            {
+                rep.code(401).send(`The session id was not found`)
+            }
+           else  if(!err)
+            {
+                rep.code(200).send({status:"success"});
+                console.log(res.rowCount);
+            } else 
+            {
+                console.log(err.message);
+            }
+        })
+    })
+};
